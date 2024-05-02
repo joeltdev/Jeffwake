a592970ae4b742bf09d8555066e0b3eac0c5d9bd
Joel Thomas Mathew <joelmathew857@gmail.com>
2024-05-02 14:30:21 +0530

    added mongodb

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3001;
 // Set up EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json);
 
 // Middleware for serving static files
 app.use(express.static(path.join(__dirname, "public")));
